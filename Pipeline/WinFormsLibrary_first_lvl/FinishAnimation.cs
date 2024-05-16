266e856789ee1d5375a918d8a4cff3bfe89fd5b0
Денис Козачок <ipz224_kdp@student.ztu.edu.ua>
2024-05-16 18:47:26 +0300

    Реалізовано патерн Фабрики це дозволить зосередитися на функціональності класу FinishAnimation, а створення зображень буде відокремлено від його логіки

--- Pipeline/WinFormsLibrary_first_lvl/FinishAnimation.cs
+++ Pipeline/WinFormsLibrary_first_lvl/FinishAnimation.cs
@@ -7,40 +7,47 @@ using System.Windows.Forms;
 
 namespace WinFormsLibrary_Levels
 {
+    public class ImageFactory
+    {
+        private string imgFolder = "D:\\Навчання_2_курс\\6-lab KPZ\\Pipeline\\img\\img for animation";
+
+        private string[] imageFileNames = {
+        "zognuta_truba_1.png", "zognuta_truba_2.png", "zognuta_truba_3.png", "zognuta_truba_4.png",
+        "troina_truba_1.png", "troina_truba_2.png", "troina_truba_3.png", "troina_truba_4.png",
+        "prama_truba_1.png", "prama_truba_2.png",
+        "chetwerna_truba.png",
+        "start-finish_truba.png"
+    };
+
+        public Image GetImage(int index)
+        {
+            if (index >= 0 && index < imageFileNames.Length)
+            {
+                string imagePath = Path.Combine(imgFolder, imageFileNames[index]);
+                return Image.FromFile(imagePath);
+            }
+            else
+            {
+                Console.WriteLine($"Invalid index for image: {index}");
+                return null;
+            }
+        }
+    }
+
     public class FinishAnimation
     {
+        private ImageFactory imageFactory = new ImageFactory();
 
         public void FinishAnim(PictureBox[] pictureBoxes, int[] finanim)
         {
-            string imgFolder = "D:\\Навчання_2_курс\\6-lab KPZ\\Pipeline\\img\\img for animation";
-
-            string[] imageFileNames = {
-                "zognuta_truba_1.png", "zognuta_truba_2.png", "zognuta_truba_3.png", "zognuta_truba_4.png",
-                "troina_truba_1.png", "troina_truba_2.png", "troina_truba_3.png", "troina_truba_4.png",
-                "prama_truba_1.png", "prama_truba_2.png",
-                "chetwerna_truba.png",
-                "start-finish_truba.png"
-            };
-
-            Image[] images = new Image[imageFileNames.Length];
-
-            for (int i = 0; i < imageFileNames.Length; i++)
-            {
-                string imagePath = Path.Combine(imgFolder, imageFileNames[i]);
-                images[i] = Image.FromFile(imagePath);
-            }
-
             for (int i = 0; i < finanim.Length; i++)
             {
-                if (finanim[i] >= 0 && finanim[i] < images.Length)
+                Image image = imageFactory.GetImage(finanim[i]);
+                if (image != null)
                 {
                     pictureBoxes[i].Image?.Dispose();
-                    pictureBoxes[i].Image = images[finanim[i]];
+                    pictureBoxes[i].Image = image;
                     pictureBoxes[i].Refresh();
-                }
-                else
-                {
-                    Console.WriteLine($"Invalid index for animation: {finanim[i]}");
                 }
             }
         }
