da1efd0edef6277ce76c691af77da42744ea0308
Денис Козачок <ipz224_kdp@student.ztu.edu.ua>
2024-05-16 18:58:42 +0300

    Розділення фабричного класа в окремий клас

--- Pipeline/WinFormsLibrary_first_lvl/FinishAnimation.cs
+++ Pipeline/WinFormsLibrary_first_lvl/FinishAnimation.cs
@@ -7,32 +7,6 @@ using System.Windows.Forms;
 
 namespace WinFormsLibrary_Levels
 {
-    public class ImageFactory
-    {
-        private string imgFolder = "D:\\Навчання_2_курс\\6-lab KPZ\\Pipeline\\img\\img for animation";
-
-        private string[] imageFileNames = {
-        "zognuta_truba_1.png", "zognuta_truba_2.png", "zognuta_truba_3.png", "zognuta_truba_4.png",
-        "troina_truba_1.png", "troina_truba_2.png", "troina_truba_3.png", "troina_truba_4.png",
-        "prama_truba_1.png", "prama_truba_2.png",
-        "chetwerna_truba.png",
-        "start-finish_truba.png"
-    };
-
-        public Image GetImage(int index)
-        {
-            if (index >= 0 && index < imageFileNames.Length)
-            {
-                string imagePath = Path.Combine(imgFolder, imageFileNames[index]);
-                return Image.FromFile(imagePath);
-            }
-            else
-            {
-                Console.WriteLine($"Invalid index for image: {index}");
-                return null;
-            }
-        }
-    }
 
     public class FinishAnimation
     {
